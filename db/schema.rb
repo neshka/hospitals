1e1063f7d62fe44b53f0184ce5c3bba1adc316a2
neshka <neshka_mind@yahoo.com>
2016-10-19 19:36:06 +0100

    create department model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017155700) do
+ActiveRecord::Schema.define(version: 20161019181957) do
+
+  create_table "departments", force: :cascade do |t|
+    t.string   "department_name", limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
 
   create_table "doctors", force: :cascade do |t|
     t.string   "first_name",    limit: 255
